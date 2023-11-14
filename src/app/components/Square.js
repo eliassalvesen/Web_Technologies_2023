fe0e7bec3958981da675f037cae799297b6d6265
Elias Salvesen <71264857+eliassalvesen@users.noreply.github.com>
2023-11-14 19:34:29 +0200

    L3 video

---
+++ src/app/components/Square.js
@@ -0,0 +1,10 @@
+export default function Square(props){
+    const sStyle={
+        width: '100px',
+        height: '150px',
+        backgroundColor: props.sColor
+    }
+    return (<div style={sStyle}>
+
+    </div>)
+}
